c7838d4f626554e5b7d789a471afd4d2f819fc88
gunyu1019 <gunyu1019@gmail.com>
2026-02-11 01:25:12 +0900

    [Fix] Add flutter_localization to i18n.

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:momeet/core/config/app_config.dart';
 import 'package:momeet/router.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'core/providers/auth_provider.dart';
 
@@ -19,6 +20,16 @@ class MoMeetApp extends ConsumerWidget {
 
     return MaterialApp.router(
       title: AppConfig.appName,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,   // Material 위젯 내부 텍스트 번역
+        GlobalWidgetsLocalizations.delegate,    // 기본 위젯의 텍스트 방향 등 설정
+        GlobalCupertinoLocalizations.delegate,   // iOS 스타일 위젯 번역
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+        Locale('en', 'US'),
+      ],
+      locale: const Locale('ko', 'KR'),
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
