0ca2935dbb9e9c5f5525a4c1764653e6b6928fd2
jjh4450 <jjh4450git@gmail.com>
2026-03-31 03:15:32 +0900

    [Refactor] mypage_page 위젯 파일 분리

--- lib/features/mypage/mypage.dart
+++ lib/features/mypage/mypage.dart
@@ -5,3 +5,9 @@ export 'presentation/pages/mypage_page.dart';
 
 // Providers
 export 'presentation/providers/my_page_providers.dart';
+
+// Widgets
+export 'presentation/widgets/profile_section.dart';
+export 'presentation/widgets/activity_stats_section.dart';
+export 'presentation/widgets/tag_stats_section.dart';
+export 'presentation/widgets/settings_section.dart';
